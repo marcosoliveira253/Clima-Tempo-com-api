2c805eee61ae92a34a0cc9163bd2b821b49f9856
marcosoliveira_dev <96296979+marcosoliveira253@users.noreply.github.com>
2023-10-14 23:37:59 -0300

    aplicação concluida.

--- script.js
+++ script.js
@@ -10,17 +10,20 @@ const cityElement = document.querySelector('#city')
 const tempElement = document.querySelector('#temperature span')
 const descElement = document.querySelector('#description')
 const weatherIconElement = document.querySelector('#weather-icon')
-const countryElement = document.querySelector('#country')
-const humidityElement = document.querySelector('#umidity span')
+//const countryElement = document.querySelector('#country')
+const humidityElement = document.querySelector('#humidity span')
 const windElement = document.querySelector('#wind span')
 
+const weatherContainer = document.querySelector("#weather-data");
+
 // Funções
+//consulta dados da api.
 const getWeatherData = async(city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`
 
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
-
+    //console.log(data)
     return data;
 };
 
@@ -31,9 +34,11 @@ const showWeatherData = async (city) => {
     tempElement.innerText = parseInt(data.main.temp);
     descElement.innerText = data.weather[0].description;
     weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    countryElement.setAttribute("src", apicountryURL + data.sys.country);
+    //countryElement.setAttribute("src", apicountryURL + data.sys.country);
     humidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide");
 };
 
 //Eventos
@@ -44,3 +49,11 @@ searchBtn.addEventListener("click", (e) => {
 
     showWeatherData(city);
 });
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+})
